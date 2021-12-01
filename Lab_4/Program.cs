a0281477c64d9293300f3a5d66085bf2d8e872df
Vera <Vera@LAPTOP-2PS2I9DA>
2021-12-01 22:33:21 +0300

    Добавьте файлы проекта.

---
+++ Lab_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число");
+            uint n= (uint)Convert.ToInt32(Console.ReadLine());
+            for (uint i = 1, kv=i; i <= n; ++i,kv=kv+2*i-1)
+            {
+                Console.WriteLine("квадрат числа {0} равен{1}", i, kv);
+                Console.ReadKey();
+            }
+
+
+        }
+    }
+}
